51daf79ae7ee607e87cc643f0c76bb143ef23ca1
Mark Haskamp <mrhask@gmail.com>
2010-02-21 12:51:33 -0500

    created Activity_Controller

--- time-machinery/app/time_machinery.rb
+++ time-machinery/app/time_machinery.rb
@@ -1,36 +1,21 @@
 require 'rubygems'
 require 'sinatra'
 require 'mongo'
+require 'controllers/Activity_Controller.rb'
 
 get '/' do
   erb :index
 end
 
 get '/create' do
-  
-  colFoo = connection()
-  colFoo.insert("name" => params["activity_name"])
+
+  Activity_Controller.createActivity params
   "created at #{Time.now} <br />activity_name: #{params['activity_name']}"
 end
 
 
 get '/list' do
-    
-  colFoo = connection()
-    
-  document = ""
-  colFoo.find("name" => /.*/).each { |doc|
-      
-    document << "&nbsp;-&nbsp; #{doc["name"]}<br/>"
-  }
-    
-  "Read back verification<br />#{document}"
+
+  Activity_Controller.showList params
 end
 
-def connection
-
-  m = Mongo::Connection.new
-  db = m.db("foo")
-  db["foo"]
-end
-    
