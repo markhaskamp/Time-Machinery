ce85eef8054aa9b050f6da26c98ec3824ad44f1a
Mark Haskamp <mrhask@gmail.com>
2010-02-21 13:06:39 -0500

    some refactoring in Activity_Controller

--- time-machinery/app/controllers/Activity_Controller.rb
+++ time-machinery/app/controllers/Activity_Controller.rb
@@ -3,23 +3,34 @@ require 'mongo'
 class Activity_Controller
 
   def self.createActivity(paramHash)
-    m = Mongo::Connection.new
-    db = m.db("foo")
-    colFoo = db["foo"]
 
-    colFoo.insert("name" => paramHash["activity_name"])
+    dbSetup { |collection|
+      collection.insert("name" => paramHash["activity_name"])
+    }
+
   end
 
   def self.showList params
-    m = Mongo::Connection.new
-    db = m.db("foo")
-    colln = db["foo"]
     
     document = ""
-    colln.find("name" => /.*/).each { |doc|
-      
-      document << "&nbsp;-&nbsp; #{doc["name"]}<br/>"
+    dbSetup { |collection|
+      collection.find("name" => /.*/).each { |doc|
+        document << "&nbsp;-&nbsp; #{doc["name"]}<br/>"
+      }
     }
     return "Read back verification<br />#{document}"
+  end
+
+
+  private 
+
+  def self.dbSetup
+
+    mongoConnection = Mongo::Connection.new
+    database = mongoConnection.db("foo")
+    
+    yield database["foo"]
+
+    mongoConnection.close
   end
 end
